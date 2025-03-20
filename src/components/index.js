f10d83670e0ef161470109a49f607a5b3b840e4f
shivkc-source <shivanshu.bansal@kcglobed.com>
2025-03-20 17:01:46 +0530

    routes.js

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,10 @@ import AppFooter from './AppFooter'
 import AppHeader from './AppHeader'
 import AppHeaderDropdown from './header/AppHeaderDropdown'
 import AppSidebar from './AppSidebar'
-
+// import DocsComponents from './DocsComponents'
+// import DocsIcons from './DocsIcons'
+// import DocsLink from './DocsLink'
+ import DocsExample from './DocsExample'
 
 export {
   AppBreadcrumb,
@@ -13,5 +16,8 @@ export {
   AppHeader,
   AppHeaderDropdown,
   AppSidebar,
- 
+  // DocsComponents,
+  // DocsIcons,
+  // DocsLink,
+   DocsExample,
 }
--- src/routes.js
+++ src/routes.js
@@ -2,25 +2,13 @@ import React from 'react'
 
 const Dashboard = React.lazy(() => import('./views/pages/Dashboard/Dashboard'))
 const Reports = React.lazy(() => import('./views/pages/Reports/Reports'))
-
-
-
-
-
-
-
-// Icons
-
-
 const Widgets = React.lazy(() => import('./views/widgets/Widgets'))
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', element: Dashboard },
   { path: '/table', name: 'Reports', element: Reports },
-  
-  
-  { path: '/widgets', name: 'Widgets', element: widgets },
+  { path: '/widgets', name: 'Widgets', element: Widgets },
 ]
 
 export default routes
