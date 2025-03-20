38bee049441d3223377f36fac82e0ed18d748ae1
shivkc-source <shivanshu.bansal@kcglobed.com>
2025-03-20 14:54:33 +0530

    insta start

---
+++ src/_nav.js
@@ -0,0 +1,196 @@
+import React from 'react'
+import CIcon from '@coreui/icons-react'
+import {
+  cilBell,
+  cilCalculator,
+  cilChartPie,
+  cilCursor,
+  cilDescription,
+  cilDrop,
+  cilExternalLink,
+  cilNotes,
+  cilPencil,
+  cilPuzzle,
+  cilSpeedometer,
+  cilStar,
+} from '@coreui/icons'
+import { CNavGroup, CNavItem, CNavTitle } from '@coreui/react'
+
+const _nav = [
+  {
+    component: CNavItem,
+    name: 'Home',
+    to: '/dashboard',
+    icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
+  
+  },
+  {
+    component: CNavItem,
+    name: 'Reports',
+    to: '/reports',
+    icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
+   
+  },
+  {
+    component: CNavItem,
+    name: 'Badge',
+    to: '/badges',
+    icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
+  
+  },
+
+ 
+  {
+    component: CNavGroup,
+    name: 'Course',
+    to: '/buttons',
+    icon: <CIcon icon={cilCursor} customClassName="nav-icon" />,
+    items: [
+      {
+        component: CNavItem,
+        name: 'Upload Videos ',
+        to: '/buttons/buttons',
+      },
+      {
+        component: CNavItem,
+        name: 'Category',
+        to: '/buttons/button-groups',
+      },
+      {
+        component: CNavItem,
+        name: 'Topic',
+        to: '/buttons/dropdowns',
+      },
+      {
+        component: CNavItem,
+        name: 'Manage E books',
+        to: '/buttons/dropdowns',
+      },
+      
+    ],
+  },
+  {
+    component: CNavItem,
+    name: 'Manage Subscription',
+    to: '/badges',
+    icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
+   
+  },
+  {
+    component: CNavItem,
+    name: 'Manage Staff',
+    to: '/badges',
+    icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
+  
+  },
+
+
+  {
+    component: CNavGroup,
+    name: 'Institutes',
+    to: '/buttons',
+    icon: <CIcon icon={cilCursor} customClassName="nav-icon" />,
+    items: [
+      {
+        component: CNavItem,
+        name: 'Central Universities',
+        to: '/buttons/buttons',
+      },
+      {
+        component: CNavItem,
+        name: 'State Universities',
+        to: '/buttons/button-groups',
+      },
+      {
+        component: CNavItem,
+        name: 'Private Universities',
+        to: '/buttons/dropdowns',
+      },
+      {
+        component: CNavItem,
+        name: 'Affiliated Colleges',
+        to: '/buttons/dropdowns',
+      },
+      
+    ],
+  },
+
+
+  {
+    component: CNavItem,
+    name: 'Manage Permissions',
+    to: '/badges',
+    icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
+   
+  },
+
+  {
+    component: CNavItem,
+    name: 'Manage User Payments',
+    to: '/badges',
+    icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
+   
+  },
+  {
+    component: CNavItem,
+    name: 'Manage Payment Plans',
+    to: '/badges',
+    icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
+   
+  },
+  {
+    component: CNavItem,
+    name: 'Manage Offers',
+    to: '/badges',
+    icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
+   
+  },
+  {
+    component: CNavItem,
+    name: 'Subscribe Users',
+    to: '/badges',
+    icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
+   
+  },
+  {
+    component: CNavItem,
+    name: 'Free Trail',
+    to: '/badges',
+    icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
+   
+  },
+  
+  {
+    component: CNavItem,
+    name: 'Manage News',
+    to: '/badges',
+    icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
+   
+  },
+  {
+    component: CNavItem,
+    name: 'Manage Blogs',
+    to: '/badges',
+    icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
+   
+  },
+  {
+    component: CNavItem,
+    name: 'Manage Reels',
+    to: '/badges',
+    icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
+   
+  },
+  {
+    component: CNavItem,
+    name: 'Manage Events',
+    to: '/badges',
+    icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
+   
+  },
+  
+  
+  
+]
+
+export default _nav
---
+++ src/components/AppSidebar.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+import {
+  CCloseButton,
+  CSidebar,
+  CSidebarBrand,
+  CSidebarFooter,
+  CSidebarHeader,
+  CSidebarToggler,
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+
+import { AppSidebarNav } from './AppSidebarNav'
+import navigation from '../_nav'
+
+// Import the logo properly
+import logo from 'src/assets/images/logo.png'
+
+const AppSidebar = () => {
+  const dispatch = useDispatch()
+  const unfoldable = useSelector((state) => state.sidebarUnfoldable)
+  const sidebarShow = useSelector((state) => state.sidebarShow)
+
+  return (
+    <CSidebar
+      className="border-end"
+      colorScheme="dark"
+      position="fixed"
+      unfoldable={unfoldable}
+      visible={sidebarShow}
+      onVisibleChange={(visible) => {
+        dispatch({ type: 'set', sidebarShow: visible })
+      }}
+    >
+      <CSidebarHeader className="border-bottom d-flex align-items-center justify-content-between">
+        <CSidebarBrand to="/" className="d-flex align-items-center">
+          <img src={logo} alt="Logo" style={{ height: 52, width: 200 }} />
+        </CSidebarBrand>
+
+        <CCloseButton
+          className="d-lg-none"
+          dark
+          onClick={() => dispatch({ type: 'set', sidebarShow: false })}
+        />
+      </CSidebarHeader>
+
+      <AppSidebarNav items={navigation} />
+
+      <CSidebarFooter className="border-top d-none d-lg-flex">
+        <CSidebarToggler
+          onClick={() => dispatch({ type: 'set', sidebarUnfoldable: !unfoldable })}
+        />
+      </CSidebarFooter>
+    </CSidebar>
+  )
+}
+
+export default React.memo(AppSidebar)
