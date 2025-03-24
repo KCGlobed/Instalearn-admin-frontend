698f4f9294cf770da8d9628f6543c4728d8fdf71
HimanshuYadav1811 <himanshuyadav181995@gmail.com>
2025-03-24 17:32:43 +0530

    merge code

--- src/routes.js
+++ src/routes.js
@@ -12,11 +12,8 @@ const Widgets = React.lazy(() => import('./views/widgets/Widgets'));
 const Report = React.lazy(() => import('./views/pages/Reports/Reports'));
 const Course = React.lazy(() => import('./views/pages/Course/Course'));
 const Videos = React.lazy(() => import('./views/pages/Course/Videos'));
-const Dashboard = React.lazy(() => import('./views/pages/Dashboard/Dashboard'))
-const Reports = React.lazy(() => import('./views/pages/Reports/Reports'))
-const Widgets = React.lazy(() => import('./views/widgets/Widgets'))
-const Report = React.lazy(() => import('./views/pages/Reports/Reports'))
-const Badge = React.lazy(() => import('./views/pages/Badge/index'))
+const Badge =React.lazy(() => import('./views/pages/Badge/index'))
+
 
 
 const routes = [
@@ -27,14 +24,8 @@ const routes = [
   { path: '/reports', name: 'Report', element: <PrivateRoute element={<Report />} /> },
   { path: '/course', name: 'Course', element: <PrivateRoute element={<Course />} /> },
   { path: '/videos', name: 'Course', element: <PrivateRoute element={<Videos />} /> },
+  { path: '/badge', name: 'Badge', element: <PrivateRoute element={<Badge />} /> },
 ];
-  { path: '/', exact: true, name: 'Home' },
-  { path: '/dashboard', name: 'Dashboard', element: Dashboard },
-  { path: '/table', name: 'Reports', element: Reports },
-  { path: '/widgets', name: 'Widgets', element: Widgets },
-  { path: '/table', name: 'Report', element: Report },
-  { path: '/badge', name: 'Badge', element: Badge },
-]
 
 export default routes;
 
